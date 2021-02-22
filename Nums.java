d654dfb7729a3aca515b7de728c3a68b434fbafd
SpanDin <SpanDu@DESKTOP-T2NUA4P.(none)>
2021-02-22 15:14:35 +0530

    Nums Commit

---
+++ Nums.java
@@ -0,0 +1,14 @@
+
+public class Nums {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("SpanDin");
+		int x = 5;
+		int y = 6;
+		int sum = x + y;
+		System.out.println(sum);
+
+	}
+
+}
