951594172174f660bf7c2bb7a2451ea51428bf60
saisrispandana <79159088+saisrispandana@users.noreply.github.com>
2021-02-22 16:06:08 +0530

    Mul Java file added

---
+++ Mul.java
@@ -0,0 +1,13 @@
+
+public class Mul {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x = 5;
+		int y = 6;
+		int mul = x * y;
+		System.out.println(mul);
+	}
+
+}
