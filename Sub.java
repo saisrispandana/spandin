0bd5bd25262a88e504b259087e2a5574183231d9
saisrispandana <79159088+saisrispandana@users.noreply.github.com>
2021-02-22 16:17:42 +0530

    Sub commit

---
+++ Sub.java
@@ -0,0 +1,13 @@
+
+public class Sub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		int x = 6;
+		int y = 3;
+		int sub = x - y;
+		System.out.println(sub);
+	}
+
+}
